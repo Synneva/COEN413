2ef0ba946ec28abb822bc8158102e086bb976cac
hakimsheriff <hakimsheriff@users.noreply.github.com>
2021-04-20 02:30:38 -0400

    Added some stuff to checker and scoreboard
    It ain't much but we definitely understand what we need to do a lot more.

--- TOP/ENV/scoreboard.sv
+++ TOP/ENV/scoreboard.sv
@@ -31,29 +31,40 @@ class scoreboard;
 
 	function void expected_output(transaction tr, output_transaction otr);
 		otr.out_tag = tr.tag;
+		logic [32:0] sum;
 		case(tr.cmd)
-
 			NOOP:	begin
 						otr.out_resp = 0;
 					  	otr.out_data = 0;
 					end
 
 			ADD:	begin		// add + overflow logic
-
+                		sum = tr.data1 + tr.data2;
+						if (sum[32])
+							otr.out_data = sum[31:0];
+							otr.out_resp = 2;
+						else
+							otr.out_data = sum[31:0];
+							otr.out_resp = 1;
 					end
 
 			SUB:	begin		// sub + underflow logic
-
+						if(tr.data1 >= tr.data2)
+							otr.out_data = tr.data1 - tr.data2;
+							otr.out_resp = 2;
+						else
+							otr.out_data = tr.data1 - tr.data2;
+							otr.out_resp = 1;
 					end
 
 			LSL:	begin
 						otr.out_resp = 1;
-						otr.out_data = tr.data1 << tr.data2;	// gotta check if this works
+						otr.out_data = tr.data1 << tr.data2[27:31];	// gotta check if this works
 					end
 
 			LSR:	begin
 						otr.out_resp = 1;
-						otr.out_data = tr.data1 >> tr.data2;
+						otr.out_data = tr.data1 >> tr.data2[27:31];
 					end
 
 			default:begin
