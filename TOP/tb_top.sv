d770657a716094ac24a707e7a6544c36363bfd17
Synneva <synneva-rcf@hotmail.com>
2021-04-21 00:39:21 -0400

    changed interface

--- TOP/tb_top.sv
+++ TOP/tb_top.sv
@@ -34,7 +34,7 @@ module tb_top;
 	
 
 	// interface instances???? can i do this idk
-	virtual calc_if intf [NUM_PORTS];
+	calc_if intf(clk,reset);
 
 	// test instance
 	tests t1(intf);
@@ -43,31 +43,31 @@ module tb_top;
 	calc2_top DUT(	.c_clk(clk),
 					.reset(reset),
 
-					.req1_data_in(intf[0].data_in),
-					.req2_data_in(intf[1].data_in),
-					.req3_data_in(intf[2].data_in),
-					.req4_data_in(intf[3].data_in),
-					.req1_cmd_in(intf[0].cmd_in),
-					.req2_cmd_in(intf[1].cmd_in),
-					.req3_cmd_in(intf[2].cmd_in),
-					.req4_cmd_in(intf[3].cmd_in),
-					.req1_tag_in(intf[0].tag_in),
-					.req2_tag_in(intf[1].tag_in),
-					.req3_tag_in(intf[2].tag_in),
-					.req4_tag_in(intf[3].tag_in),
+					.req1_data_in(intf.in_port[0].data_in),
+					.req2_data_in(intf.in_port[1].data_in),
+					.req3_data_in(intf.in_port[2].data_in),
+					.req4_data_in(intf.in_port[3].data_in),
+					.req1_cmd_in(intf.in_port[0].cmd_in),
+					.req2_cmd_in(intf.in_port[1].cmd_in),
+					.req3_cmd_in(intf.in_port[2].cmd_in),
+					.req4_cmd_in(intf.in_port[3].cmd_in),
+					.req1_tag_in(intf.in_port[0].tag_in),
+					.req2_tag_in(intf.in_port[1].tag_in),
+					.req3_tag_in(intf.in_port[2].tag_in),
+					.req4_tag_in(intf.in_port[3].tag_in),
 
-					.out_data1(intf[0].out_data),
-					.out_data2(intf[1].out_data),
-					.out_data3(intf[2].out_data),
-					.out_data4(intf[3].out_data),
-					.out_resp1(intf[0].out_resp),
-					.out_resp2(intf[1].out_resp),
-					.out_resp3(intf[2].out_resp),
-					.out_resp4(intf[3].out_resp),
-					.out_tag1(intf[0].out_tag),
-					.out_tag2(intf[1].out_tag),
-					.out_tag3(intf[2].out_tag),
-					.out_tag4(intf[3].out_tag)
+					.out_data1(intf.out_port[0].out_data),
+					.out_data2(intf.out_port[1].out_data),
+					.out_data3(intf.out_port[2].out_data),
+					.out_data4(intf.out_port[3].out_data),
+					.out_resp1(intf.out_port[0].out_resp),
+					.out_resp2(intf.out_port[1].out_resp),
+					.out_resp3(intf.out_port[2].out_resp),
+					.out_resp4(intf.out_port[3].out_resp),
+					.out_tag1(intf.out_port[0].out_tag),
+					.out_tag2(intf.out_port[1].out_tag),
+					.out_tag3(intf.out_port[2].out_tag),
+					.out_tag4(intf.out_port[3].out_tag)
 				);
 
 	// enable dump?
